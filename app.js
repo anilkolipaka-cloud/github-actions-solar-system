c65794bdcff218df44e799470513055130594728
Anilkolipaka <aniltech1212@gmail.com>
2025-03-22 20:07:34 +0530

    added workflow level env variables

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect(process.env.MONGO_URI, {
-    user: SuperUser,
-    pass: SuperPassword,
+mongoose.connect('mongodb+srv://supercluster.d83jj.mongodb.net/superData', {
+    user: process.env.MONGO_USERNAME,
+    pass: process.env.MONGO_PASSWORD,
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, function(err) {
